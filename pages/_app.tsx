a5e6d2f4f79ffb303473873ab354ab8a677c8162
Chrispine Abala <ashiruma@gmail.com>
2025-08-28 06:12:48 +0300

    Final fix: Remove legacy pages directory

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css'; // <-- **THIS LINE IS THE FIX**
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
